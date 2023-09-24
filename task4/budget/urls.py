bcaefea8106c96f9ea3684605b26a6cd064e2eee
Paweł Kirszniok <pawelkirszniok@gmail.com>
2023-09-24 21:58:35 +0100

    feat: add Income endpoints

--- task4/budget/urls.py
+++ task4/budget/urls.py
@@ -1,4 +1,4 @@
-from budget.views import BudgetViewSet, ExpenseViewSet
+from budget.views import BudgetViewSet, ExpenseViewSet, IncomeViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
@@ -8,6 +8,7 @@ router.register('', BudgetViewSet)
 
 nested_router = routers.NestedDefaultRouter(router, '', lookup='budget')
 nested_router.register('expenses', ExpenseViewSet, basename='budget-expenses')
+nested_router.register('incomes', IncomeViewSet, basename='budget-incomes')
 
 
 urlpatterns = [
